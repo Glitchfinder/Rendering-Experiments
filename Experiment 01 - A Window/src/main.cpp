5e07b5fd5f48562e45029b5352f26f3c23f4ae43
Sean Porter <glitchkey@gmail.com>
2014-01-28 14:29:14 -0800

    First experiment: Actually making a window.

---
+++ Experiment 01 - A Window/src/main.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+// GLEW must be included before gl.h or glfw.h
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+	// Initialize GLFW
+	if (!glfwInit()) {
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		return -1;
+	}
+
+	// Create an OpenGL window
+	glfwWindowHint(GLFW_SAMPLES, 4); // 4x AA
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// We don't want the deprecated OpenGL
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// Set the window name
+	const char* name = "Experiment 01 - A Window";
+	// Create the window with OpenGL
+	GLFWwindow*  window = glfwCreateWindow(640, 480, name, NULL, NULL);
+	// Check to see if the window opened successfully
+	if (!window) {
+		fprintf(stderr, "Failed to open GLFW window\n");
+		glfwTerminate();
+		return -1;
+	}
+
+	// Set the context as current
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		//return -1;
+	}
+
+	// Set up an actual game loop
+	bool   running = true;
+	time_t lastSync;
+	time_t currentTime;
+	double timeSinceLastRefresh;
+	time(&lastSync);
+
+	while (running) {
+		// Check timing
+		time(&currentTime);
+		timeSinceLastRefresh = 1000.0 * difftime(currentTime, lastSync);
+
+		// Update graphics if appropriate
+		if (timeSinceLastRefresh >= 0.016) { // 16MS = 60FPS
+			// We're not drawing anything yet
+
+			// Swap buffers
+			glfwSwapBuffers(window);
+		}
+
+		// Poll events
+		glfwPollEvents();
+
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS ||
+			glfwWindowShouldClose(window))
+		{
+			running = false;
+		}
+	}
+}
---
+++ README.txt
@@ -0,0 +1,10 @@
+== Glitchfinder's Rendering Experiments ==
+
+  Source:        https://github.com/Glitchfinder/Rendering-Experiments
+  Issue Tracker: https://github.com/Glitchfinder/Rendering-Experiments/issues
+
+This repository is mostly to serve as a backup and archive as I teach myself
+about rendering via OpenGL. I make no guarantees regarding quality, quantity,
+or usability. The code found within this repository is licensed under the MIT
+license when permitted, and the appropriate license will be included in the
+project directory when not.
